a458d96a02d21cfd64d73baedf77936b038965e3
sanakaco <390691027@qq.com>
2019-11-23 15:06:11 +0800

    创建Eureka服务器

---
+++ CloudMicroServer/EurekaServer/src/main/java/com/kin/Cloud/Eureka/ServerApplication.java
@@ -0,0 +1,19 @@
+package com.kin.Cloud.Eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author pandas
+ * @ClassName ServerApplication
+ * @Description TODO
+ * @create 2019/11/23 14:56
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class,args);
+    }
+}
